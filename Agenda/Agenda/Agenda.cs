6dd35dfec846ded473f72ee1b7c039637f8a3ddb
Angel Salvador <02angelsalvaodor@gmail.com>
2018-04-20 20:21:37 -0500

    Agenda

---
+++ Agenda/Agenda/Agenda.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Agenda
+    {
+        private Contactos[] agenda = new Contactos[15];
+        private int pos = 0;
+        
+        public void agregar(Contactos newContacto)
+        {
+            agenda[pos] = newContacto;
+        }
+     
+        public void insertar(Contactos insertar, int lugar)
+        {
+            for (int i = 14; i > lugar; i--)
+            {
+                agenda[i] = agenda[i--];
+            }
+            agenda[lugar] = insertar;
+        }
+        public Contactos buscar(long tel)
+        {
+            for (int i = 0; i <= pos; i++)
+            {
+                if (agenda[i].Telefono == tel)
+                {
+                    return agenda[i];
+                }
+            }
+            return null;
+        }
+
+        public bool eliminar(long tel)
+        {
+            bool van = false;
+            for (int i = 0; i <= pos; i++)
+            {
+                if (agenda[i].Telefono == tel)
+                {
+                    for (int j = 0; j < 14; j++)
+                    {
+                        agenda[j] = agenda[j+1];
+                    }
+                    van = true;
+                }
+            }
+            
+            return van;
+        }    
+
+        public string listar()
+        {
+
+            return " ";
+        }
+    }
+}
---
+++ Agenda/Agenda/Contactos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    public class Contactos
+    {
+        private
+        string _nombre;
+        string _apPaterno;
+        string _apMaterno;
+        string _correo;
+        long _telefono;
+        int _edad;
+
+
+        public string Nombre{
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string ApellidoPaterno{
+            get { return _apPaterno; }
+            set { _apPaterno = value; }
+        }
+        public string ApellidoMaterno{
+            get { return _apMaterno; }
+            set { _apMaterno = value; }
+        }
+        public string Correo{
+            get { return _correo; }
+            set { _correo = value; }    
+        }
+        public long Telefono {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+        public int Edad {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+    }
+}
